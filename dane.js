c679350e11132836577f4038a89431d2e0ef074a
CodeNoob67 <ostinszczygiel@gmail.com>
2026-08-05 22:00:38 +0200

    Update dane.js

--- dane.js
+++ dane.js
@@ -7,13 +7,11 @@ const BOHATEROWIE = [
         moc: "Wzmocnione widły",
         opis:
             "Farmer walczy na krótkim dystansie. Co trzeci atak zadaje 50% więcej obrażeń.",
-
         hp: 115,
         damage: 18,
         speed: 3.4,
         range: 330,
         cooldown: 650,
-
         ticketCost: 0
     },
 
@@ -24,14 +22,12 @@ const BOHATEROWIE = [
         rzadkosc: "rzadki",
         moc: "Ostatnia tarcza",
         opis:
-            "Rycerz walczy wręcz. Gdy ma mało życia, jego tarcza pochłania 30% otrzymywanych obrażeń.",
-
+            "Rycerz walczy wręcz. Gdy ma mało życia, jego tarcza pochłania 30% obrażeń.",
         hp: 205,
         damage: 31,
         speed: 2.8,
         range: 88,
         cooldown: 650,
-
         ticketCost: 8
     },
 
@@ -43,14 +39,28 @@ const BOHATEROWIE = [
         moc: "Błyskawiczna strzała",
         opis:
             "Łucznik atakuje z dużego dystansu. Co trzecia strzała porusza się znacznie szybciej.",
-
         hp: 145,
         damage: 30,
         speed: 3.4,
         range: 720,
         cooldown: 750,
-
         ticketCost: 12
+    },
+
+    {
+        id: "ksiadz",
+        nazwa: "Ksiądz",
+        ikona: "⛪",
+        rzadkosc: "bardzo-rzadki",
+        moc: "Przywołanie ducha",
+        opis:
+            "Co 3 sekundy przywołuje słabszego ducha, który ściga najbliższego wroga i atakuje go.",
+        hp: 185,
+        damage: 25,
+        speed: 3.4,
+        range: 300,
+        cooldown: 700,
+        ticketCost: 16
     },
 
     {
@@ -60,14 +70,12 @@ const BOHATEROWIE = [
         rzadkosc: "epicki",
         moc: "Desperacki atak",
         opis:
-            "Goblin jest szybkim wojownikiem walczącym wręcz. Poniżej 50% życia zadaje 25% więcej obrażeń.",
-
+            "Goblin jest szybkim wojownikiem wręcz. Poniżej 50% życia zadaje 25% więcej obrażeń.",
         hp: 105,
         damage: 28,
         speed: 4.4,
         range: 76,
         cooldown: 480,
-
         ticketCost: 18
     },
 
@@ -78,15 +86,29 @@ const BOHATEROWIE = [
         rzadkosc: "epicki",
         moc: "Kamienna fala",
         opis:
-            "Golem jest bardzo wytrzymały, ale powolny. Co piąty atak tworzy falę uderzeniową.",
-
+            "Golem jest bardzo wytrzymały i powolny. Co piąty atak tworzy falę uderzeniową.",
         hp: 330,
         damage: 38,
         speed: 2.1,
         range: 350,
         cooldown: 950,
-
         ticketCost: 26
+    },
+
+    {
+        id: "duch",
+        nazwa: "Duch",
+        ikona: "👻",
+        rzadkosc: "epicki",
+        moc: "Przenikanie",
+        opis:
+            "Duch jest szybkim wojownikiem walczącym wręcz i może przechodzić przez ściany.",
+        hp: 90,
+        damage: 40,
+        speed: 4.5,
+        range: 78,
+        cooldown: 520,
+        ticketCost: 30
     },
 
     {
@@ -96,14 +118,12 @@ const BOHATEROWIE = [
         rzadkosc: "mityczny",
         moc: "Magiczna klątwa",
         opis:
-            "Co czwarty atak Czarodzieja nakłada klątwę. Przeklęty przeciwnik przez 3 sekundy otrzymuje 20% więcej obrażeń.",
-
+            "Co czwarty atak nakłada klątwę. Przeklęty przeciwnik przez 3 sekundy otrzymuje 20% więcej obrażeń.",
         hp: 145,
         damage: 39,
         speed: 3.4,
         range: 690,
         cooldown: 820,
-
         ticketCost: 34
     },
 
@@ -115,13 +135,11 @@ const BOHATEROWIE = [
         moc: "Potrójny królewski atak",
         opis:
             "Król przy każdym ataku wystrzeliwuje trzy pociski jednocześnie.",
-
         hp: 155,
         damage: 32,
         speed: 3,
         range: 580,
         cooldown: 850,
-
         ticketCost: 42
     },
 
@@ -132,14 +150,12 @@ const BOHATEROWIE = [
         rzadkosc: "legendarny",
         moc: "Magiczny strumień",
         opis:
-            "Wiedźma strzela bez przerwy małymi kulami energii. Pociski poruszają się ekstremalnie szybko.",
-
+            "Wiedźma bardzo szybko strzela małymi złotymi kulami energii.",
         hp: 105,
         damage: 7,
         speed: 4,
         range: 750,
-        cooldown: 0,
-
+        cooldown: 50,
         ticketCost: 50
     }
 ];
