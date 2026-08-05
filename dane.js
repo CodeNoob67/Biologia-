cebefc938a9777262d14280caeb51cb28ad0e68b
CodeNoob67 <ostinszczygiel@gmail.com>
2026-08-05 20:44:46 +0200

    Update dane.js

--- dane.js
+++ dane.js
@@ -3,14 +3,17 @@ const BOHATEROWIE = [
         id: "farmer",
         nazwa: "Farmer",
         ikona: "🧑‍🌾",
+        rzadkosc: "startowy",
         moc: "Wzmocnione widły",
         opis:
-            "Walczący na krótkim dystansie farmer. Co trzeci atak zadaje 50% więcej obrażeń.",
+            "Farmer walczy na krótkim dystansie. Co trzeci atak zadaje 50% więcej obrażeń.",
+
         hp: 115,
         damage: 18,
         speed: 3.4,
         range: 330,
         cooldown: 650,
+
         ticketCost: 0
     },
 
@@ -18,14 +21,17 @@ const BOHATEROWIE = [
         id: "rycerz",
         nazwa: "Rycerz",
         ikona: "🛡️",
+        rzadkosc: "rzadki",
         moc: "Ostatnia tarcza",
         opis:
-            "Silny wojownik walczący wręcz. Przy małej ilości życia otrzymuje tarczę pochłaniającą 30% obrażeń.",
+            "Rycerz walczy wręcz. Gdy ma mało życia, jego tarcza pochłania 30% otrzymywanych obrażeń.",
+
         hp: 205,
         damage: 31,
         speed: 2.8,
         range: 88,
         cooldown: 650,
+
         ticketCost: 8
     },
 
@@ -33,14 +39,17 @@ const BOHATEROWIE = [
         id: "lucznik",
         nazwa: "Łucznik",
         ikona: "🏹",
+        rzadkosc: "rzadki",
         moc: "Błyskawiczna strzała",
         opis:
-            "Atakuje z bardzo dużego dystansu. Co trzecia strzała leci znacznie szybciej.",
+            "Łucznik atakuje z dużego dystansu. Co trzecia strzała porusza się znacznie szybciej.",
+
         hp: 145,
         damage: 30,
         speed: 3.4,
         range: 720,
         cooldown: 750,
+
         ticketCost: 12
     },
 
@@ -48,14 +57,17 @@ const BOHATEROWIE = [
         id: "goblin",
         nazwa: "Goblin",
         ikona: "👺",
+        rzadkosc: "epicki",
         moc: "Desperacki atak",
         opis:
-            "Bardzo szybki wojownik walczący wręcz. Poniżej połowy życia zadaje 25% więcej obrażeń.",
+            "Goblin jest szybkim wojownikiem walczącym wręcz. Poniżej 50% życia zadaje 25% więcej obrażeń.",
+
         hp: 105,
         damage: 28,
         speed: 4.4,
         range: 76,
         cooldown: 480,
+
         ticketCost: 18
     },
 
@@ -63,14 +75,17 @@ const BOHATEROWIE = [
         id: "golem",
         nazwa: "Golem",
         ikona: "🗿",
+        rzadkosc: "epicki",
         moc: "Kamienna fala",
         opis:
-            "Powolny i bardzo wytrzymały. Co piąty atak wysyła falę uderzeniową.",
+            "Golem jest bardzo wytrzymały, ale powolny. Co piąty atak tworzy falę uderzeniową.",
+
         hp: 330,
         damage: 38,
         speed: 2.1,
         range: 350,
         cooldown: 950,
+
         ticketCost: 26
     },
 
@@ -78,14 +93,17 @@ const BOHATEROWIE = [
         id: "czarodziej",
         nazwa: "Czarodziej",
         ikona: "🧙‍♂️",
+        rzadkosc: "mityczny",
         moc: "Magiczna klątwa",
         opis:
-            "Co czwarty atak nakłada klątwę. Przeklęty przeciwnik przez 3 sekundy otrzymuje 20% więcej obrażeń.",
+            "Co czwarty atak Czarodzieja nakłada klątwę. Przeklęty przeciwnik przez 3 sekundy otrzymuje 20% więcej obrażeń.",
+
         hp: 145,
         damage: 39,
         speed: 3.4,
         range: 690,
         cooldown: 820,
+
         ticketCost: 34
     },
 
@@ -93,14 +111,17 @@ const BOHATEROWIE = [
         id: "krol",
         nazwa: "Król",
         ikona: "🤴",
+        rzadkosc: "legendarny",
         moc: "Potrójny królewski atak",
         opis:
-            "Legendarny wojownik, który przy każdym ataku wystrzeliwuje trzy pociski.",
+            "Król przy każdym ataku wystrzeliwuje trzy pociski jednocześnie.",
+
         hp: 155,
         damage: 32,
         speed: 3,
         range: 580,
         cooldown: 850,
+
         ticketCost: 42
     },
 
@@ -108,14 +129,17 @@ const BOHATEROWIE = [
         id: "wiedzma",
         nazwa: "Wiedźma",
         ikona: "🧙‍♀️",
+        rzadkosc: "legendarny",
         moc: "Magiczny strumień",
         opis:
-            "Strzela bez przerwy małymi kulami energii, które poruszają się niemal natychmiastowo.",
+            "Wiedźma strzela bez przerwy małymi kulami energii. Pociski poruszają się ekstremalnie szybko.",
+
         hp: 105,
         damage: 7,
         speed: 4,
         range: 750,
         cooldown: 0,
-        ticketCost: 250
+
+        ticketCost: 50
     }
 ];
