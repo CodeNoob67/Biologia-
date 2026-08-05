a25cd3b210ef4adbecee27dec6b7cff84964daa5
CodeNoob67 <ostinszczygiel@gmail.com>
2026-08-05 14:53:58 +0200

    Create dane.js

---
+++ dane.js
@@ -0,0 +1,57 @@
+const BOHATEROWIE = [
+    {
+        id: "rycerz",
+        nazwa: "Sir Aldren",
+        ikona: "🛡️",
+        moc: "Szarża tarczą",
+        opis: "Wytrzymały rycerz walczący mieczem. Jego atak odpycha przeciwnika.",
+        hp: 150,
+        damage: 22,
+        speed: 3.2,
+        range: 56,
+        cooldown: 430,
+        ticketCost: 0
+    },
+
+    {
+        id: "luczniczka",
+        nazwa: "Elara",
+        ikona: "🏹",
+        moc: "Deszcz strzał",
+        opis: "Szybka łuczniczka atakująca przeciwników z dużej odległości.",
+        hp: 95,
+        damage: 18,
+        speed: 3.8,
+        range: 240,
+        cooldown: 520,
+        ticketCost: 8
+    },
+
+    {
+        id: "mag",
+        nazwa: "Mordren",
+        ikona: "🧙",
+        moc: "Kula ognia",
+        opis: "Potężny mag, który wystrzeliwuje duże kule ognia.",
+        hp: 85,
+        damage: 32,
+        speed: 3,
+        range: 210,
+        cooldown: 760,
+        ticketCost: 14
+    },
+
+    {
+        id: "berserker",
+        nazwa: "Bjorn",
+        ikona: "🪓",
+        moc: "Szał wojownika",
+        opis: "Wolniejszy, ale bardzo silny wojownik walczący toporem.",
+        hp: 185,
+        damage: 38,
+        speed: 2.7,
+        range: 62,
+        cooldown: 850,
+        ticketCost: 20
+    }
+];
